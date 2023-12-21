5750e1b39de1bf488ea8443423801cf946a0aa88
Matt Straathof <matt.straat@gmail.com>
2023-12-21 09:49:38 -0800

    feat: add unary data client with get/set support

--- lib/src/messages/values.dart
+++ lib/src/messages/values.dart
@@ -1,13 +1,38 @@
-sealed class Value {}
+import 'dart:convert';
+
+sealed class Value {
+  List<int> toBinary();
+  String toUtf8();
+}
 
 class StringValue implements Value {
   final String _value;
   StringValue(String v) : _value = v;
   String get value => _value;
+
+  @override
+  toBinary() {
+    return utf8.encode(_value);
+  }
+
+  @override
+  toUtf8() {
+    return _value;
+  }
 }
 
 class BinaryValue implements Value {
   final List<int> _value;
   BinaryValue(List<int> v) : _value = v;
   List<int> get value => _value;
+
+  @override
+  List<int> toBinary() {
+    return _value;
+  }
+
+  @override
+  String toUtf8() {
+    return utf8.decode(_value);
+  }
 }
