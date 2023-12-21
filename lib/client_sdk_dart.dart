6088ed8cadd67b8c6e779d73e03cbe6dc0b26d90
Anita Ruangrotsakun <138700973+anitarua@users.noreply.github.com>
2023-12-21 10:00:28 -0800

    chore: demo flutter chat app (#34)
    * chore: flutter demo chat app
    
    * make app run on ios and macos
    
    * full app working on macos and ios
    
    * update readme
    
    * dart format
    
    * exclude flutter from dart analysis

--- lib/client_sdk_dart.dart
+++ lib/client_sdk_dart.dart
@@ -4,6 +4,12 @@
 library;
 
 export 'src/topic_client.dart';
+export 'src/auth/credential_provider.dart';
+export 'src/config/topic_configurations.dart';
+export 'src/messages/values.dart';
+export 'src/messages/responses/topics/topic_publish.dart';
+export 'src/messages/responses/topics/topic_subscribe.dart';
+export 'src/messages/responses/topics/topic_subscription_item.dart';
 export 'src/config/logger.dart' show LogLevel;
 
 // TODO: Export any libraries intended for clients of this package.
