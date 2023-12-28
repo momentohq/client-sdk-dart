b66a052997108f25663c3683a7d285281c223eb3
anitarua <anita@momentohq.com>
2023-12-28 13:17:20 -0800

    use switch expression instead of switch statement

--- examples/topics/basic_subscriber.dart
+++ examples/topics/basic_subscriber.dart
@@ -7,34 +7,33 @@ void main() async {
       CredentialProvider.fromEnvironmentVariable("MOMENTO_API_KEY"),
       Mobile.latest());
 
-  var sub = topicClient.subscribe("cache", "topic");
-  switch (sub) {
-    case TopicSubscription():
-      print("Successful subscription!");
+  var subscription = topicClient.subscribe("cache", "topic");
+  var messageStream = switch (subscription) {
+    TopicSubscription() => subscription.stream,
+    TopicSubscribeError() => throw Exception(
+        "Subscribe error: ${subscription.errorCode} ${subscription.message}"),
+  };
 
-      // cancel subscription 15 seconds from now
-      Timer(const Duration(seconds: 15), () {
-        print("Cancelling subscription!");
-        sub.unsubscribe();
-      });
+  // cancel subscription 15 seconds from now
+  Timer(const Duration(seconds: 15), () {
+    print("Cancelling subscription!");
+    subscription.unsubscribe();
+  });
 
-      try {
-        await for (final msg in sub.stream) {
-          switch (msg) {
-            case TopicSubscriptionItemBinary():
-              print("Binary value: ${msg.value}");
-            case TopicSubscriptionItemText():
-              print("String value: ${msg.value}");
-            case TopicSubscriptionItemError():
-              print("Error receiving message: ${msg.errorCode}");
-          }
-        }
-      } catch (e) {
-        print("Runtime type: ${e.runtimeType}");
-        print("Error with await for loop: $e");
+  try {
+    await for (final msg in messageStream) {
+      switch (msg) {
+        case TopicSubscriptionItemBinary():
+          print("Binary value: ${msg.value}");
+        case TopicSubscriptionItemText():
+          print("String value: ${msg.value}");
+        case TopicSubscriptionItemError():
+          print("Error receiving message: ${msg.errorCode}");
       }
-    case TopicSubscribeError():
-      print("Subscribe error: ${sub.errorCode} ${sub.message}");
+    }
+  } catch (e) {
+    print("Runtime type: ${e.runtimeType}");
+    print("Error with await for loop: $e");
   }
 
   topicClient.close();
