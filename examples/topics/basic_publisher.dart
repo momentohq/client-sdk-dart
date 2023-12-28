2e462d1c327c04f0b5af882f20ae1ce75bdd75f9
anitarua <anita@momentohq.com>
2023-12-28 10:03:35 -0800

    dart format

--- examples/topics/basic_publisher.dart
+++ examples/topics/basic_publisher.dart
@@ -8,7 +8,8 @@ void main() async {
 
   // publish 10 messages spaced 1 second apart
   for (final i in Iterable.generate(10)) {
-    var result = await topicClient.publish("cache", "topic", StringValue("hi $i"));
+    var result =
+        await topicClient.publish("cache", "topic", StringValue("hi $i"));
     switch (result) {
       case TopicPublishSuccess():
         print("Successful publish!");
