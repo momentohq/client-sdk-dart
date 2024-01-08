39ddfbce20361cf86bfbca0def31fc302ef407f0
Pete Gautier <pete@momentohq.com>
2024-01-08 14:39:39 -0800

    chore: adding docstrings for public entities

--- lib/src/cache_client.dart
+++ lib/src/cache_client.dart
@@ -7,7 +7,6 @@ import 'package:momento/src/internal/utils/validators.dart';
 
 import 'config/logger.dart';
 
-// import 'config/logger.dart';
 
 abstract class ICacheClient {
   Future<CreateCacheResponse> createCache(String cacheName);
@@ -31,6 +30,14 @@ class CacheClient implements ICacheClient {
   late final ControlClient _controlClient;
   final MomentoLogger _logger = MomentoLogger('MomentoCacheClient');
 
+  /// Client to perform operations against Momento Serverless Cache.
+  ///
+  /// ```dart
+  /// final cacheClient = CacheClient(
+  ///   CredentialProvider.fromEnvironmentVariable("MOMENTO_API_KEY"),
+  ///   MobileCacheConfiguration.latest(),
+  ///   Duration(seconds: 30));
+  /// ```
   CacheClient(CredentialProvider credentialProvider,
       CacheClientConfiguration configuration, Duration defaultTtl) {
     _dataClient = DataClient(credentialProvider, configuration, defaultTtl);
@@ -39,6 +46,19 @@ class CacheClient implements ICacheClient {
     _logger.trace("initializing cache client");
   }
 
+  /// Creates a cache with the name [cacheName] if it doesn't exist.
+  ///
+  /// Returns a response that can be resolved to one of its possible types:
+  /// ```dart
+  /// switch (createResponse) {
+  ///   case CreateCacheError():
+  ///     print("Got an error: ${createResponse.errorCode} ${createResponse.message}");
+  ///   case CreateCacheSuccess():
+  ///     print("Cache was created");
+  ///   case AlreadyExists():
+  ///     print("Cache already exists");
+  /// }
+  /// ```
   @override
   Future<CreateCacheResponse> createCache(String cacheName) {
     try {
@@ -54,6 +74,18 @@ class CacheClient implements ICacheClient {
     return _controlClient.createCache(cacheName);
   }
 
+
+  /// Deletes a cache with the name [cacheName].
+  ///
+  /// Returns a response that can be resolved to one of its possible types:
+  /// ```dart
+  /// switch (deleteResponse) {
+  ///   case DeleteCacheError():
+  ///     print("Got an error: ${deleteResponse.errorCode} ${deleteResponse.message}");
+  ///   case DeleteCacheSuccess():
+  ///     print("Cache was deleted");
+  /// }
+  /// ```
   @override
   Future<DeleteCacheResponse> deleteCache(String cacheName) {
     try {
@@ -69,11 +101,35 @@ class CacheClient implements ICacheClient {
     return _controlClient.deleteCache(cacheName);
   }
 
+  /// Lists all caches.
+  ///
+  /// Returns a response that can be resolved to one of its possible types:
+  /// ```dart
+  /// switch(listResponse) {
+  ///   case ListCachesSuccess():
+  ///     print("My caches: ${listResponse.cacheNames.join(", ")}");
+  ///   case ListCachesError():
+  ///     print("Got an error: ${listResponse.errorCode} ${listResponse.message}");
+  /// }
+  /// ```
   @override
   Future<ListCachesResponse> listCaches() {
     return _controlClient.listCaches();
   }
 
+  /// Gets the cache value stored for a given key.
+  ///
+  /// Returns a response that can be resolved to one of its possible types:
+  /// ```dart
+  /// switch(getResponse) {
+  ///   case GetHit():
+  ///     print("Get value from cache: ${getResponse.value}");
+  ///   case GetMiss():
+  ///     print("Value for key was not found in cache.");
+  ///   case GetError():
+  ///     print("Got an error: ${getResponse.errorCode} ${getResponse.message}");
+  /// }
+  /// ```
   @override
   Future<GetResponse> get(String cacheName, Value key) {
     try {
@@ -89,6 +145,18 @@ class CacheClient implements ICacheClient {
     return _dataClient.get(cacheName, key);
   }
 
+  /// Associates the given key with the given value.
+  ///
+  /// If a value for the key is already present it is replaced with the new value.
+  /// Returns a response that can be resolved to one of its possible types:
+  /// ```dart
+  /// switch(setResponse) {
+  ///   case SetSuccess():
+  ///     print("Key was set to value");
+  ///   case SetError():
+  ///     print("Got an error: ${setResponse.errorCode} ${setResponse.message}");
+  /// }
+  /// ```
   @override
   Future<SetResponse> set(String cacheName, Value key, Value value,
       {Duration? ttl}) {
@@ -105,6 +173,18 @@ class CacheClient implements ICacheClient {
     return _dataClient.set(cacheName, key, value, ttl: ttl);
   }
 
+  /// Removes the given key from the cache.
+  ///
+  /// The key can represent a single value or a collection.
+  /// Returns a response that can be resolved to one of its possible types:
+  /// ```dart
+  /// switch(deleteResponse) {
+  ///   case DeleteSuccess():
+  ///     print("Key deleted from cache");
+  ///   case DeleteError():
+  ///     print("Got an error: ${deleteResponse.errorCode} ${deleteResponse.message}");
+  /// }
+  /// ```
   @override
   Future<DeleteResponse> delete(String cacheName, Value key) {
     try {
@@ -120,6 +200,9 @@ class CacheClient implements ICacheClient {
     return _dataClient.delete(cacheName, key);
   }
 
+  /// Close the client and free up all associated resources.
+  ///
+  /// NOTE: the client object will not be usable after calling this method.
   @override
   Future<void> close() async {
     await _dataClient.close();
