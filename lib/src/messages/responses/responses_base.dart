af7a1858bfd2494ab46971dfbe777a2eab3bab80
anitarua <anita@momentohq.com>
2024-01-09 15:17:35 -0800

    chore: error responses should print informative errors by default

--- lib/src/messages/responses/responses_base.dart
+++ lib/src/messages/responses/responses_base.dart
@@ -1,6 +1,12 @@
 import '../../errors/errors.dart';
 
-class ErrorResponseBase extends AbstractExceptionResponseBase {
+class ErrorResponseBase extends AbstractExceptionResponseBase
+    implements Exception {
   ErrorResponseBase(SdkException exception)
       : super(exception.message, exception.errorCode, exception.innerException);
+
+  @override
+  String toString() {
+    return "[${super.errorCode}] ${super.message}";
+  }
 }
